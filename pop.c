68c0faf0dc5891a2cc3e868d277ef48f2085771e
SMBABAYO <smbabayo@gmail.com>
2023-12-14 10:21:41 -0800

    all

--- pop.c
+++ pop.c
@@ -8,16 +8,18 @@
  */
 void f_pop(stack_t **head, unsigned int counter)
 {
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-
-	stack_t *temp = *head;
-	*head = temp->next;
-	free(temp);
+    stack_t *temp;
+    
+    if (*head == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+    
+    temp = *head;
+    *head = temp->next;
+    free(temp);
 }
